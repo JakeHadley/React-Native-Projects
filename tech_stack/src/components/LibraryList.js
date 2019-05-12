4862bbb92350d3bbfca0ea2afa00f42550e954df
Jake Hadley <jakehadley10@gmail.com>
2019-05-11 23:51:23 -0600

    Finished TechStack

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+  renderItem(library) {
+    return <ListItem library={library} />;
+  }
+
   render() {
-    console.log(this.props);
-    return;
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={library => library.id.toString()}
+      />
+    );
   }
 }
 
 const mapStateToProps = state => {
-  return { hey: state.libraries };
+  return { libraries: state.libraries };
 };
 
 export default connect(mapStateToProps)(LibraryList);
---
+++ tech_stack/src/components/ListItem.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { LayoutAnimation, NativeModules, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
+const { UIManager } = NativeModules;
+UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true)
+
+class ListItem extends Component {
+  componentWillReceiveProps() {
+    LayoutAnimation.spring();
+  }
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={{ flex: 1 }}>{library.item.description}</Text>
+        </CardSection>
+      )
+    }
+  }
+
+  render() {
+    const { id, title } = this.props.library.item;
+    const { titleStyle } = styles;
+
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>{title}</Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
+    )
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.item.id;
+
+  return { expanded };
+}
+
+export default connect(mapStateToProps, actions)(ListItem); //first argument to connect is specifically mapStateToProps, the second is actions
