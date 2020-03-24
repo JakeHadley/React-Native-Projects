9577a2ccd5a97d6e53a2f72ddc6b2c6a529e2df0
Jake Hadley <jakehadley10@gmail.com>
2020-03-24 15:27:37 -0700

    Added server.

---
+++ track-server/src/index.js
@@ -0,0 +1,35 @@
+require('./models/User');
+require('./models/Track');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express();
+
+app.use(bodyParser.json()); //this is to extract the email and password from the post request, puts any request into json format
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri = 'mongodb+srv://pianoman730:pianoman1031@cluster0-uiubp.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+})
+mongoose.connection.on('error', error => {
+  console.error('Error connecting to mongo', error);
+});
+
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email is ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+})
